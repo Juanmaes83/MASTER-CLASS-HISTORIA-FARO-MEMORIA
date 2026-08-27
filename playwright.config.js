7825c5b63ad0cfd83fd67ff588098b9ac9d40235
Juanmaes83 <juanmaes83@gmail.com>
2026-08-27 09:40:53 +0200

    Run Faro WebGL QA serially in Chromium

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,8 @@
-module.exports = {
-  testDir: './tests',
-  timeout: 45000,
-  use: {
-    baseURL: 'http://127.0.0.1:4173',
-    headless: true,
-    screenshot: 'only-on-failure'
-  },
-  reporter: [['list'], ['html', { outputFolder: 'playwright-report', open: 'never' }]]
+module.exports={
+  testDir:'./tests',
+  timeout:120000,
+  workers:1,
+  fullyParallel:false,
+  use:{baseURL:'http://127.0.0.1:4173',headless:true,screenshot:'only-on-failure'},
+  reporter:[['list'],['html',{outputFolder:'playwright-report',open:'never'}]]
 };
